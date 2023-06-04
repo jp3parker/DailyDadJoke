623034aeb8576de299b48e4c32e30212ba14e772
jp3parker <jp3parker@gmail.com>
2023-06-04 03:11:50 -0500

    everything is set up with crontab and this program should be run once a day. The script will get a random joke from an api and commit that joke to github.

--- main.sh
+++ main.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 joke=$((curl -H "Accept: application/json" https://icanhazdadjoke.com/) | jq '.joke')
+
 date=$(date "+%m-%d-%Y")
 
-echo -e "$date\n$joke\n\n" >> text.txt
+echo -e "$date\n$joke\n\n" | sed 's/\\//g' >> text.txt
 
 git add text.txt
 git commit -m "$date's joke"
